13027339caa91041950d6a4a1135ea8754f4cfa4
Syo Igarashi <igara1119@gmail.com>
2024-11-27 15:58:06 +0900

    Updates

--- assets/PresenterPrint-820dd48c.js
+++ assets/PresenterPrint-820dd48c.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as o,h as a,F as g,r as v,n as x,j as y,o as i,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-244720ca.js";import{N as V}from"./NoteDisplay-50effe7b.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as o,h as a,F as g,r as v,n as x,j as y,o as i,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-97d9f607.js";import{N as V}from"./NoteDisplay-ef7a0d25.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
